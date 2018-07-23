6fd04abca3ecb4784d343ea7725e03ab240eba32
Mostefaoui <m.mostefaoui@hotmail.fr>
2018-07-23 09:13:55 +0100

    Add config service projects

---
+++ src/Spike.Istio.Ambassador.ConfigService.V1/Controllers/ConfigsController.cs
@@ -0,0 +1,16 @@
+﻿namespace Spike.Istio.Ambassador.ConfigService.V1.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ConfigsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> GetAllConfigs()
+        {
+            return new[] {"v1:config1", "v1:config2", "v1:config3"};
+        }
+    }
+}
---
+++ src/Spike.Istio.Ambassador.ConfigService.V2/Controllers/ConfigsController.cs
@@ -0,0 +1,16 @@
+﻿namespace Spike.Istio.Ambassador.ConfigService.V2.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ConfigsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "v2:config1", "v2:config2", "v2:config3" };
+        }
+    }
+}
